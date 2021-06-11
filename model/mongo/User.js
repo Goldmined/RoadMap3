3cd8dedf37f4694c70c5eaf9628461bb93f9fbe5
Daria <goldmined666@gmail.com>
2021-06-11 17:51:23 +0300

    Task3_1

--- model/mongo/User.js
+++ model/mongo/User.js
@@ -1,30 +1,32 @@
-const mongoose = require( mongoose );
+
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const CompanySchema = new Schema({
-    name : String,
-    catchPhrase : String,
-    bs : String
+    name: String,
+    catchPhrase: String,
+    bs: String
 });
 
 const UserSchema = new Schema({
-     id : Number,
-     name : String,
-     username : String,
-     email : String,
-     address : {
-       street : String,
-       suite : String,
-       city : String,
-       zipcode : String,
-       geo : {
-         lat : Number,
-         lng : Number
-      }
-    },
-     phone : String,
-     website : String,
-     company: CompanySchema
+        id: Number,
+        name: String,
+        username: String,
+        email: String,
+        address: {
+          street: String,
+          suite: String,
+          city: String,
+          zipcode: String,
+          geo: {
+            lat: Number,
+            lng: Number
+          }
+        },
+        phone: String,
+        website: String,
+        company: CompanySchema
+});
 
-const model = mongoose.model( User , UserSchema,  users );
+const model = mongoose.model('User', UserSchema, 'users');
 
-module.exports = model;
+module.exports = model
